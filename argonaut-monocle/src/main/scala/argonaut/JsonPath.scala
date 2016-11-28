9f962609d3dc63bcd217594763cefa3392a8f2d8
Julien Truffaut <truffaut.julien@gmail.com>
2016-11-28 21:44:52 +0000

    add JsonFoldPath, filter, unsaferFilter, unsafeAs and rename arrFilter, objFilter to filterByIndex and filterByField

--- argonaut-monocle/src/main/scala/argonaut/JsonPath.scala
+++ argonaut-monocle/src/main/scala/argonaut/JsonPath.scala
@@ -4,7 +4,7 @@ import argonaut.JsonMonocle._
 import argonaut.JsonObjectMonocle._
 import monocle.function.{At, FilterIndex, Index}
 import monocle.std.list._
-import monocle.{Optional, Traversal}
+import monocle.{Fold, Optional, Prism, Traversal}
 
 import scala.language.dynamics
 
@@ -23,22 +23,31 @@ final case class JsonPath(json: Optional[Json, Json]) extends Dynamic {
   def obj: Optional[Json, JsonObject] = json composePrism jObjectPrism
 
   def at(field: String): Optional[Json, Option[Json]] =
-    json.composePrism(jObjectPrism).composeLens(At.at(field))
+    json composePrism jObjectPrism composeLens At.at(field)
 
   def selectDynamic(field: String): JsonPath =
-    JsonPath(json.composePrism(jObjectPrism).composeOptional(Index.index(field)))
+    JsonPath(json composePrism jObjectPrism composeOptional Index.index(field))
 
   def index(i: Int): JsonPath =
-    JsonPath(json.composePrism(jArrayPrism).composeOptional(Index.index(i)))
+    JsonPath(json composePrism jArrayPrism composeOptional Index.index(i))
 
   def each: JsonTraversalPath =
     JsonTraversalPath(json composeTraversal jDescendants)
 
-  def arrFilter(p: Int => Boolean): JsonTraversalPath =
+  def filterByIndex(p: Int => Boolean): JsonTraversalPath =
     JsonTraversalPath(arr composeTraversal FilterIndex.filterIndex(p))
 
-  def objFilter(p: String => Boolean): JsonTraversalPath =
+  def filterByField(p: String => Boolean): JsonTraversalPath =
     JsonTraversalPath(obj composeTraversal FilterIndex.filterIndex(p))
+
+  final def filter(p: Json => Boolean): JsonFoldPath =
+    JsonFoldPath(unsafeFilter(p).json.asFold)
+
+  final def unsafeFilter(p: Json => Boolean): JsonPath =
+    JsonPath(json composePrism UnsafeOptics.select(p))
+
+  final def unsafeAs[A: CodecJson]: Optional[Json, A] =
+    json composePrism UnsafeOptics.parse
 }
 
 object JsonPath {
@@ -60,20 +69,87 @@ final case class JsonTraversalPath(json: Traversal[Json, Json]) extends Dynamic 
   def obj: Traversal[Json, JsonObject] = json composePrism jObjectPrism
 
   def at(field: String): Traversal[Json, Option[Json]] =
-    json.composePrism(jObjectPrism).composeLens(At.at(field))
+    json composePrism jObjectPrism composeLens At.at(field)
 
   def selectDynamic(field: String): JsonTraversalPath =
-    JsonTraversalPath(json.composePrism(jObjectPrism).composeOptional(Index.index(field)))
+    JsonTraversalPath(json composePrism jObjectPrism composeOptional Index.index(field))
 
   def index(i: Int): JsonTraversalPath =
-    JsonTraversalPath(json.composePrism(jArrayPrism).composeOptional(Index.index(i)))
+    JsonTraversalPath(json composePrism jArrayPrism composeOptional Index.index(i))
 
   def each: JsonTraversalPath =
     JsonTraversalPath(json composeTraversal jDescendants)
 
-  def arrFilter(p: Int => Boolean): JsonTraversalPath =
+  def filterByIndex(p: Int => Boolean): JsonTraversalPath =
     JsonTraversalPath(arr composeTraversal FilterIndex.filterIndex(p))
 
-  def objFilter(p: String => Boolean): JsonTraversalPath =
+  def filterByField(p: String => Boolean): JsonTraversalPath =
     JsonTraversalPath(obj composeTraversal FilterIndex.filterIndex(p))
+
+  final def filter(p: Json => Boolean): JsonFoldPath =
+    JsonFoldPath(unsafeFilter(p).json.asFold)
+
+  final def unsafeFilter(p: Json => Boolean): JsonTraversalPath =
+    JsonTraversalPath(json composePrism UnsafeOptics.select(p))
+
+  final def unsafeAs[A: CodecJson]: Traversal[Json, A] =
+    json composePrism UnsafeOptics.parse
+}
+
+final case class JsonFoldPath(json: Fold[Json, Json]) extends Dynamic {
+  final def `null`: Fold[Json, Unit] = json composePrism jNullPrism
+  final def boolean: Fold[Json, Boolean] = json composePrism jBoolPrism
+  final def byte: Fold[Json, Byte] = json composePrism jBytePrism
+  final def short: Fold[Json, Short] = json composePrism jShortPrism
+  final def int: Fold[Json, Int] = json composePrism jIntPrism
+  final def long: Fold[Json, Long] = json composePrism jLongPrism
+  final def bigInt: Fold[Json, BigInt] = json composePrism jBigIntPrism
+  final def bigDecimal: Fold[Json, BigDecimal] = json composePrism jBigDecimalPrism
+  final def number: Fold[Json, JsonNumber] = json composePrism jNumberPrism
+  final def string: Fold[Json, String] = json composePrism jStringPrism
+  final def arr: Fold[Json, List[Json]] = json composePrism jArrayPrism
+  final def obj: Fold[Json, JsonObject] = json composePrism jObjectPrism
+
+  final def at(field: String): Fold[Json, Option[Json]] =
+    json composePrism jObjectPrism composeLens At.at(field)
+
+  final def selectDynamic(field: String): JsonFoldPath =
+    JsonFoldPath(json composePrism jObjectPrism composeOptional Index.index(field))
+
+  final def index(i: Int): JsonFoldPath =
+    JsonFoldPath(json composePrism jArrayPrism composeOptional Index.index(i))
+
+  final def each: JsonFoldPath =
+    JsonFoldPath(json composeTraversal jDescendants)
+
+  final def filterByIndex(p: Int => Boolean): JsonFoldPath =
+    JsonFoldPath(arr composeTraversal FilterIndex.filterIndex(p))
+
+  final def filterByField(p: String => Boolean): JsonFoldPath =
+    JsonFoldPath(obj composeTraversal FilterIndex.filterIndex(p))
+
+  final def filter(p: Json => Boolean): JsonFoldPath =
+    JsonFoldPath(json composePrism UnsafeOptics.select(p))
+
+  final def as[A: CodecJson]: Fold[Json, A] =
+    json composePrism UnsafeOptics.parse
+}
+
+object UnsafeOptics {
+  /**
+    * Decode a value at the current location.
+    *
+    * Note that this operation is not lawful, since decoding is not injective.
+    */
+  def parse[A](implicit codec: CodecJson[A]): Prism[Json, A] =
+    Prism[Json, A](codec.decodeJson(_).toOption)(codec.encode)
+
+  /**
+    * Select if a value matches a predicate
+    *
+    * Note that this operation is not lawful because the predicate could be invalidated with set or modify.
+    * However `select(_.a > 10) composeLens b` is safe because once we zoom into `b`, we cannot change `a` anymore.
+    */
+  def select[A](p: A => Boolean): Prism[A, A] =
+    Prism[A, A](a => if (p(a)) Some(a) else None)(identity)
 }
--- argonaut-monocle/src/test/scala/argonaut/JsonPathSpecification.scala
+++ argonaut-monocle/src/test/scala/argonaut/JsonPathSpecification.scala
@@ -2,8 +2,14 @@ package argonaut
 
 import argonaut.Argonaut._
 import org.specs2.mutable.Specification
+import argonaut.JsonPath.root
 
 object JsonPathSpecification extends Specification {
+
+  case class Car(model: String, maxSpeed: Int, automatic: Boolean)
+  object  Car {
+    implicit val codec: CodecJson[Car] = CodecJson.casecodec3(Car.apply, Car.unapply)("model", "maxSpeed", "automatic")
+  }
 
   val john: Json = Json.obj(
     "first_name" -> "John".asJson,
@@ -29,34 +35,71 @@ object JsonPathSpecification extends Specification {
 
   "JsonPath" >> {
     "support traversal by field name" >> {
-      JsonPath.root.address.street_number.int.getOption(john) must_== Some(12)
+      root.address.street_number.int.getOption(john) must_== Some(12)
     }
 
     "support traversal by array index" >> {
-      JsonPath.root.cars.index(1).model.string.getOption(john) must_== Some("suv")
+      root.cars.index(1).model.string.getOption(john) must_== Some("suv")
     }
 
     "support insertion and deletion" >> {
-      JsonPath.root.at("first_name").setOption(None)(john) must_== john.obj.map(_.-("first_name")).map(jObject)
-      JsonPath.root.at("foo").set(Some(true.asJson))(john).obj.flatMap(_.apply("foo")) must_== Some(jTrue)
+      root.at("first_name").setOption(None)(john) must_== john.obj.map(_.-("first_name")).map(jObject)
+      root.at("foo").set(Some(true.asJson))(john).obj.flatMap(_.apply("foo")) must_== Some(jTrue)
+    }
+
+    "support parsing json using a codec" >> {
+      root.cars.index(0).unsafeAs[Car].getOption(john) must_== Some(Car("fancy", 120, automatic = false))
     }
   }
 
   "JsonTraversalPath" >> {
     "support traversal over each values of a json object" >> {
-      JsonPath.root.each.string.getAll(john) must_== List("John", "Doe")
+      root.each.string.getAll(john) must_== List("John", "Doe")
     }
 
     "support traversal over each values of a json array" >> {
-      JsonPath.root.cars.each.maxSpeed.int.getAll(john) must_== List(120, 80)
+      root.cars.each.maxSpeed.int.getAll(john) must_== List(120, 80)
     }
 
-    "support filtering of json object" >> {
-      JsonPath.root.objFilter(_.contains("first")).string.getAll(john) must_== List("John")
+    "support traversal over each values of a json array" >> {
+      root.cars.each.maxSpeed.int.getAll(john) must_== List(120, 80)
     }
 
-    "support filtering of json array" >> {
-      JsonPath.root.cars.arrFilter(_ % 2 == 1).model.string.getAll(john) must_== List("suv")
+    "support filtering by field of json object" >> {
+      root.filterByField(_.contains("first")).string.getAll(john) must_== List("John")
     }
+
+    "support filtering by index of json array" >> {
+      root.cars.filterByIndex(_ % 2 == 1).unsafeAs[Car].getAll(john) must_== List(Car("suv", 80, automatic = true))
+    }
+
+    "support a safe filtering by value" >> {
+      root.cars.each.filter(root.maxSpeed.int.asFold.exist(_ > 100)(_)).model.string.getAll(john) must_== List("fancy")
+    }
+
+    "support an unsafe filtering by value" >> {
+      root.cars.each.unsafeFilter(root.maxSpeed.int.asFold.exist(_ > 100)(_)).model.string.set("new")(john) must_== Json.obj(
+        "first_name" -> "John".asJson,
+        "last_name"  -> "Doe".asJson,
+        "age"        -> 25.asJson,
+        "address"    -> Json.obj(
+          "street_number" -> 12.asJson,
+          "street_name"   -> "High Street".asJson
+        ),
+        "cars" -> Json.array(
+          Json.obj(
+            "model"     -> "new".asJson,
+            "maxSpeed"  -> 120.asJson,
+            "automatic" -> false.asJson
+          ),
+          Json.obj(
+            "model"     -> "suv".asJson,
+            "maxSpeed"  -> 80.asJson,
+            "automatic" -> true.asJson
+          )
+        ).asJson
+      )
+    }
+
   }
 }
